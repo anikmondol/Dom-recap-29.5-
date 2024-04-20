a36452c98640d495da5f787ab8f2a00d8fe4a55d
anikmondol <anikmondol558363@gmil.com>
2024-04-21 00:18:35 +0600

    recap module 29.5 full done

---
+++ validation.js
@@ -0,0 +1,21 @@
+const number = '23';
+
+if(typeof number === 'number'){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+const numbers = [10, 44, 70, 32];
+
+console.log(Array.isArray(numbers));
+
+
+const student = {
+    name : 'anik',
+    roll : 55,
+    job : 'study'
+}
+
+
+console.log(typeof student);
